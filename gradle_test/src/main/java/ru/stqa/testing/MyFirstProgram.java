1eb85cbd69bf8801290a6855d243661a71f6efb8
spbmarat <marat3132@gmail.com>
2016-02-29 01:06:05 +0300

    Some math added

---
+++ gradle_test/src/main/java/ru/stqa/testing/MyFirstProgram.java
@@ -0,0 +1,14 @@
+package ru.stqa.testing;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, my Java worlds!");
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(2-2);
+        System.out.println(1/2);
+        System.out.println(1.0/2);
+    }
+}
